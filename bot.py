6c8b03f9ad02919ae4292539048255864685418f
Daniel <adeboyed93@gmail.com>
2023-03-31 08:49:47 +0100

    update

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import os
 import discord
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_member_join(member):
+    # Send a greeting message to the member
     channel = member.guild.system_channel
     if channel is not None:
         await channel.send(f"Welcome to the server, {member.mention}!")
 
-token = os.getenv("BOT_TOKEN")
 
-client.run(token)
+
+client.run(os.getenv('BOT_TOKEN'))
